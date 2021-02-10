c810141f895fee726325d542dd5df969511cce59
佐藤 開智 <revofal@gmail.com>
2021-02-10 23:27:52 +0900

    Frist Commit

---
+++ src/components/TextField.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <li v-for="data in datas" :key="data">{{ data }} </li>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: ()=>({datas:[12,12,12,13]}),
+}
+</script>
