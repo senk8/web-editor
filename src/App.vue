4615681d1e2844f0d0252a08feae8e385bf59b10
佐藤 開智 <revofal@gmail.com>
2021-02-11 19:14:48 +0900

    [ADD]routerの追加、基本レイアウトの完成。　次はエディターの入力領域の作成をする。

--- src/App.vue
+++ src/App.vue
@@ -44,64 +44,107 @@
     <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
     <v-toolbar-title>Vuetify</v-toolbar-title>
     <v-spacer></v-spacer>
+
     <v-toolbar-items>
-    <v-btn text outlined> Save </v-btn>
+    <v-btn text outlined to="/editor"> Save </v-btn>
     <v-btn text outlined> New </v-btn>
+
     <v-menu offset-y>
       <template v-slot:activator="{on}">
       <v-btn v-on="on" text>menu<v-icon>mdi-menu-down</v-icon></v-btn>
       </template>
       <v-list>
-        <v-list-item v-for="option in options" :key="option">
+        <v-list-item v-for="option in options" :key="option.name" :to="option.link">
           <v-list-item-icon>
           <v-icon>{{ option.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>{{ option.title }}</v-list-item-title>
+            <v-list-item-title>{{ option.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-menu>
+
     </v-toolbar-items>
   </v-app-bar>
+
+  <v-main>
+    <router-view />
+  </v-main>
+
   <v-footer dark app>
     Vuetify
   </v-footer>
+
   </v-app>
 </template>
+
+
 
 <script>
 //import HelloWorld from './components/HelloWorld';
 //import TextField from './components/TextField';
-
 export default {
   name: 'App',
-
   components: {
   },
-
   data: () => ({
       drawer:false,
       options:[
-        {title:"Setting",icon:"mdi-vuetify"}
-        ,{title:"Import",icon:"mdi-github"}
-        ,{title:"Export",icon:"mdi-github"}
+        {
+          name:"Setting",
+          icon:"mdi-vuetify",
+          link:"/editor"
+        }
+        ,{
+          name:"Import",
+          icon:"mdi-github",
+          link:"/about"
+        }
+        ,{
+          name:"Export",
+          icon:"mdi-github",
+          link:"/"
+        }
       ],
       nav_lists:[
         {
           name:"Setting",
           icon:"mdi-vuetify",
-          lists:['Quick Start','Pre-made layouts']
+          lists:['Quick Start','Pre-made layouts'],
         }
         ,{
           name:"Import",
-          icon:"mdi-github"
+          icon:"mdi-github",
         }
         ,{
           name:"Export",
-          icon:"mdi-github"
+          icon:"mdi-github",
         }
       ]
   }),
 };
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
